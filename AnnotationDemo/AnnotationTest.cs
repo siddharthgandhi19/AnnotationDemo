2fbd839469cf693517731579d7c53feef9c6062d
siddharth gandhi <siddharthgandhi@outlook.com>
2022-12-07 09:24:19 +0530

    [Siddharth]Add: Annotation Demo

---
+++ AnnotationDemo/AnnotationTest.cs
@@ -0,0 +1,44 @@
+﻿using Polly;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnnotationDemo
+{
+    class AnnotationTest
+    {
+        public static void Display()
+        {
+            Console.WriteLine("Employee class Validation");
+            Console.WriteLine("-----------------------------\n");
+            Employee objEmployee = new Employee();
+            objEmployee.Name = "Sid";
+            objEmployee.Age = 18;
+            objEmployee.PhoneNumber = "7894561230";
+            objEmployee.Email = "sid@gmail.com";
+
+            ValidationContext context = new ValidationContext(objEmployee, null, null);
+            List<ValidationResult> results = new List<ValidationResult>();
+            bool valid = Validator.TryValidateObject(objEmployee, context, results, true);
+
+            if( !valid )
+            {
+                foreach(ValidationResult Totalresult in results)
+                {
+                    Console.WriteLine("Member Name : {0}", Totalresult.MemberNames.First(), Environment.NewLine);
+                    Console.WriteLine("Error Msg : {0}{1}", Totalresult.ErrorMessage, Environment.NewLine);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Name: " + objEmployee.Name + "\n" + "Age: " + objEmployee.Age + "\n" + "PhoneNumber: " + objEmployee.PhoneNumber + "\n" + "Email: " + objEmployee.Email);
+            }
+            Console.WriteLine("Enter To Exit");
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ AnnotationDemo/Employee.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnnotationDemo
+{
+    public class Employee
+    {
+        [Required(ErrorMessage = "Employee {0} is required")]
+        [StringLength(100, MinimumLength =3, ErrorMessage = "Name should be minimum 3 characters and a maximum of 100 characters")]
+        [DataType(DataType.Text)]
+        public string Name { get; set; }
+        [Range(18,99, ErrorMessage = "Age should be above 18")]
+        public int Age { get; set; }
+        [DataType(DataType.PhoneNumber)]
+        [Phone]
+        public string PhoneNumber { get; set; }
+        [DataType(DataType.EmailAddress)]
+        [EmailAddress]
+        public string Email { get; set; }
+    }
+}
